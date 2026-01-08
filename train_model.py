04006873548d9d9ec1cdbf3d1522221a677d79a6
BK-Rahma <rahma.bouchnak@polytechnicien.tn>
2026-01-08 10:42:12 +0100

    fix: restart CI/CD pipeline

--- train_model.py
+++ train_model.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
